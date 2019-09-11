954d8986898302166384f0a3fd5e98d3ac468a56
nicky-code <aline.nicole7@gmail.com>
2019-09-11 08:52:57 +0200

    added the second commit.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
+
 from flask import render_template
 from app import app
+from .request import get_movies
+from .request import get_movies,get_movie
+
 
 #views
 @app.route('/')
@@ -8,15 +12,35 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    
+     # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title,popular = popular_movies)
+
+
 
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title)
 
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+# @app.route('/movie/<int:movie_id>')
+# def movie(movie_id):
+
+#     '''
+#     View movie page function that returns the movie details page and its data   
+#     '''
+#     return render_template('movie.html',id = movie_id)
+
+
+@app.route('/movie/<int:id>')
+def movie(id):
 
     '''
-    View movie page function that returns the movie details page and its data   
+    View movie page function that returns the movie details page and its data
     '''
-    return render_template('movie.html',id = movie_id)
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
