5db9cee36dc3db2b6369be4631f0e38a46ab7050
nicky-code <aline.nicole7@gmail.com>
2019-09-12 17:53:13 +0200

    added another folder.

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nicky/Desktop/watchlist/virtual"
+set -gx VIRTUAL_ENV "/home/wecode/Desktop/watchlist/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
