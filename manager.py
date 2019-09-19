c5f38f53df3151c78c7972f219f8ba09085561cb
nicky-code <aline.nicole7@gmail.com>
2019-09-19 15:17:54 +0200

    some authentication made.

--- manager.py
+++ manager.py
@@ -1,13 +1,16 @@
 
 from flask_script import Manager,Server
 from app import create_app,db
-from app.models import User
+from app.models import User,Role
+from  flask_migrate import Migrate, MigrateCommand
 
 
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 manager.add_command('server',Server)
 @manager.command
 def test():
@@ -18,7 +21,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User,Role = Role )
 
 
 if __name__ == '__main__':
